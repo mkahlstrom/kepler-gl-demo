b7aea586776126fb160c4c0aab1c25d68dfd0471
Mats Kahlström <m.kahlstrom@gmail.com>
2025-03-29 21:26:35 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+const dummyData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Location A"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-122.4194, 37.7749]  // San Francisco
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Location B"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-118.2437, 34.0522]  // Los Angeles
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Location C"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-74.0060, 40.7128]  // New York City
+      }
+    }
+  ]
+};
