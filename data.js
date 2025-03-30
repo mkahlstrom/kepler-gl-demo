17acf7a67a9eb40c40beb22ef3b59431edf2e8f8
Mats Kahlström <m.kahlstrom@gmail.com>
2025-03-30 19:53:38 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,35 +1,43 @@
 const dummyData = {
   "type": "FeatureCollection",
   "features": [
+    // USA Locations
     {
       "type": "Feature",
-      "properties": {
-        "name": "Location A"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-122.4194, 37.7749]  // San Francisco
-      }
+      "properties": { "name": "San Francisco" },
+      "geometry": { "type": "Point", "coordinates": [-122.4194, 37.7749] }
     },
     {
       "type": "Feature",
-      "properties": {
-        "name": "Location B"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-118.2437, 34.0522]  // Los Angeles
-      }
+      "properties": { "name": "Los Angeles" },
+      "geometry": { "type": "Point", "coordinates": [-118.2437, 34.0522] }
     },
     {
       "type": "Feature",
-      "properties": {
-        "name": "Location C"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-74.0060, 40.7128]  // New York City
-      }
+      "properties": { "name": "New York City" },
+      "geometry": { "type": "Point", "coordinates": [-74.0060, 40.7128] }
+    },
+
+    // Norway Locations 🇳🇴
+    {
+      "type": "Feature",
+      "properties": { "name": "Oslo" },
+      "geometry": { "type": "Point", "coordinates": [10.7522, 59.9139] }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "Bergen" },
+      "geometry": { "type": "Point", "coordinates": [5.3242, 60.3913] }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "Trondheim" },
+      "geometry": { "type": "Point", "coordinates": [10.3951, 63.4305] }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "Tromsø" },
+      "geometry": { "type": "Point", "coordinates": [18.9560, 69.6496] }
     }
   ]
 };
